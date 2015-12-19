4865ea0748e219006db4c668a33fbb03f703857c
Johnny Stenback <github@jstenback.com>
2015-12-19 13:27:26 -0800

    Update this connection code and auth code to work with endpoint https://owner-api.teslamotors.com/

--- pytesla/connection.py
+++ pytesla/connection.py
@@ -3,64 +3,33 @@ import urllib2
 import urllib
 from vehicle import Vehicle
 
-class CookieJar:
-
-    def __init__(self):
-        self._cookies = {}
-
-    def extractCookies(self, response, nameFilter = None):
-        for cookie in response.headers.getheaders('Set-Cookie'):
-            name, value = (cookie.split("=", 1) + [""])[:2]
-            if not nameFilter or name in nameFilter:
-                self._cookies[name] = value.split(";")[0]
-
-
-    def addCookie(self, name, value):
-        self._cookies[name] = value
-
-    def hasCookie(self, name):
-        return self._cookies.has_key(name)
-
-    def setCookies(self, request):
-        request.add_header('Cookie',
-                           "; ".join(["%s=%s" % (k,v)
-                                     for k,v in self._cookies.items()]))
-
-    def __repr__(self):
-        return str(self._cookies)
-
-class GHTTPCookieProcessor(urllib2.BaseHandler):
-    def __init__(self, cookieJar):
-        self.cookies = cookieJar
-
-    def https_response(self, request, response):
-        self.cookies.extractCookies(response)
-        return response
-
-    def https_request(self, request):
-        self.cookies.setCookies(request)
-        return request
-
-GHTTPCookieProcessor.http_request = GHTTPCookieProcessor.https_request
-GHTTPCookieProcessor.http_response = GHTTPCookieProcessor.https_response
-
 class Session:
     def __init__(self):
-        self._cookies = CookieJar()
+        pass
 
     def read_url(self, url, post_data = None):
         """
-        Gets the url URL with cookies enabled. Posts post_data.
+        Gets the url URL. Posts post_data.
         """
-        req = urllib2.build_opener(GHTTPCookieProcessor(self._cookies))
-        req.addheaders = [('User-agent', "pytesla")]
+        req = urllib2.build_opener()
+
+        if 'access_token' in self.state:
+            req.addheaders = [('Authorization',
+                               'Bearer ' + str(self.state['access_token']))]
+
         if post_data.__class__ == dict:
             post = urllib.urlencode(post_data)
         else:
             post = post_data
-        f = req.open(self._encode(url), data=post)
-        if f.headers.dict.has_key('set-cookie'):
-            self._cookies.extractCookies(f)
+        f = None
+        try:
+            f = req.open(self._encode(url), data=post)
+        except urllib2.HTTPError, e:
+            if e.code == 401 and e.reason == "Unauthorized":
+                del self.state['access_token']
+
+            raise e
+
         return f
 
     def read_json(self, url, post_data = None):
@@ -71,10 +40,8 @@ class Session:
         if isinstance(value, unicode):
             value = value.encode("utf-8")
         return value
-_ENDPOINT = 'https://portal.vn.teslamotors.com/'
 
-class ErrorInvalidCredentials(Exception):
-    pass
+_ENDPOINT = 'https://owner-api.teslamotors.com/'
 
 class Connection(Session):
     def __init__(self, email, passwd):
@@ -82,14 +49,23 @@ class Connection(Session):
         self.login(email, passwd)
 
     def login(self, email, passwd):
-        self.read_url(_ENDPOINT + 'login')
-        self.read_url(_ENDPOINT + 'login', {'user_session[email]' : email, 'user_session[password]' : passwd } )
-        if not self._cookies.hasCookie('user_credentials'):
-            raise ErrorInvalidCredentials()
+        cred = {}
+
+        with open(os.path.expanduser("~/.pytesla"), "r") as f:
+            cred = json.load(f)
+
+        r = self.read_json(_ENDPOINT + 'oauth/token',
+                           {'grant_type': 'password',
+                            'client_id': cred['client_id'],
+                            'client_secret': cred['client_secret'],
+                            'email' : email,
+                            'password' : passwd } )
+
+        if 'access_token' in r:
+            self.state['access_token'] = r['access_token']
 
     def read_json_path(self, path, post_data = None):
         return Session.read_json(self, _ENDPOINT + path, post_data)
-
 
     def vehicle(self, vin):
         return Vehicle(vin, self)
